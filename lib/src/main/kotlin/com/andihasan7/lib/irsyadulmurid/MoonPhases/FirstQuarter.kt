532433f6beaf7490d45a37d0a5f73b86c594a138
andihasan97 <andihasanelfalakiy@gmail.com>
2024-01-02 13:46:00 +0700

    add: menambahkan lisensi disemua file

--- lib/src/main/kotlin/com/andihasan7/lib/irsyadulmurid/MoonPhases/FirstQuarter.kt
+++ lib/src/main/kotlin/com/andihasan7/lib/irsyadulmurid/MoonPhases/FirstQuarter.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of lib-hisab-irsyadulmurid.
+ *
+ * lib-hisab-irsyadulmurid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lib-hisab-irsyadulmurid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lib-hisab-irsyadulmurid.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.andihasan7.lib.irsyadulmurid.MoonPhases
 
 import kotlin.math.round
